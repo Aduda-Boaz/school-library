a0be577af513c54b8d57155510427d548d42a3ae
Aduda-Boaz <adudaboaz@gmail.com>
2022-06-23 16:06:25 +0300

    Add book rentals to the person class

--- person.rb
+++ person.rb
@@ -1,17 +1,33 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1...100)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @book = []
+    @rentals = []
   end
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def can_use_services
     return true if is_of_age? || @parent_permission
 
     false
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(rental, book)
+    book.rentals.push(rental) unless book.rentals.include?(rental)
+    book.person = self
+    @rentals.push(rental)
+    @book.push(book)
   end
 
   private
